531f976b6b5d57374b665cbd89134e3d30292573
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2023-12-25 22:46:52 +0200

    Initial commit

---
+++ src/main/java/com/order/OrderNotificationApp/OrderNotificationApp.java
@@ -0,0 +1,13 @@
+package com.order.OrderNotificationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderNotificationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderNotificationApp.class, args);
+	}
+
+}
