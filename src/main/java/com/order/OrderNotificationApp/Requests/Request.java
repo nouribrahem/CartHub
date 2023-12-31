0d49dff2f492f1156ae58b141192646fa64c4018
Rawan Younis <ryo@luftborn.com>
2023-12-31 13:24:41 +0200

    Add Request Class

---
+++ src/main/java/com/order/OrderNotificationApp/Requests/Request.java
@@ -0,0 +1,13 @@
+package com.order.OrderNotificationApp.Requests;
+
+public class Request {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
